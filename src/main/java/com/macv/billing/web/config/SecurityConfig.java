0b0ad332ed143fadcf80e04c55032d298dfaa218
Mateo Vasco <macv9620@utp.edu.co>
2023-10-20 23:44:06 -0500

    Se configura autenticación y autorización para usuarios ADMIN y CUSTOMER

--- src/main/java/com/macv/billing/web/config/SecurityConfig.java
+++ src/main/java/com/macv/billing/web/config/SecurityConfig.java
@@ -2,8 +2,15 @@ package com.macv.billing.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -14,13 +21,49 @@ public class SecurityConfig {
                 .csrf(csrfConfig -> {
                     csrfConfig.disable();
                 })
-                //HABILITAR CORS
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(customizeRequests -> {
                     customizeRequests
+                            .requestMatchers(HttpMethod.GET, "/api/product/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/api/brand/**").permitAll()
+                            .requestMatchers(HttpMethod.GET, "/api/category/**").permitAll()
+                            .requestMatchers(HttpMethod.POST,"/api/invoice/newBuy/**").hasAnyRole("CUSTOMER", "ADMIN")
+                            .requestMatchers(HttpMethod.DELETE).denyAll()
+                            .requestMatchers("/**").hasRole("ADMIN")
                             .anyRequest()
                             .authenticated();
-                }).httpBasic(Customizer.withDefaults());
+                })
+                .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    //Será un usuario en memoria, no en una BD u otro lugar almacenado
+    @Bean
+    public UserDetailsService memoryUsers(){
+
+        UserDetails admin = User.builder()
+                .username("admin")
+                //Se codifica clave para ser guardada en memoria
+                .password(passwordEncoder().encode("testAdmin"))
+                .roles("ADMIN")
+                .build();
+
+        //Nuevo usuario y rol
+        UserDetails customer = User.builder()
+                .username("customer")
+                //Se codifica clave para ser guardada en memoria
+                .password(passwordEncoder().encode("testCustomer"))
+                .roles("CUSTOMER")
+                .build();
+
+        //Usuario en memoria
+        return new InMemoryUserDetailsManager(admin, customer);
+    }
+
+    //Encoder que provee Spring Security
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        //Tipo de codificado comunmente empleado
+        return new BCryptPasswordEncoder();
     }
 }
